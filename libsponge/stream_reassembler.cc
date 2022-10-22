d5b3ac535d00f87a8b16f7ba389635dc7135b604
rjzhb <rjzhb@arch.localdomain>
2022-10-22 17:52:28 +0800

    lab4

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -10,14 +10,12 @@
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) :
-    _output(capacity), _capacity(capacity), _map(), _set(), _idx_ptr(0), _idx_end(1e9){
-
-}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _output(capacity), _capacity(capacity), _map(), _set(), _idx_ptr(0), _idx_end(1e9) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
@@ -25,27 +23,34 @@ StreamReassembler::StreamReassembler(const size_t capacity) :
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
     size_t end = index + data.size();
 
-    if(eof) _idx_end = end;
-    //当前字符超过容量
-    if(_idx_ptr + _capacity <= index) return;
+    if (eof)
+        _idx_end = end;
+    // 当前字符超过容量
+    if (_idx_ptr + _capacity <= index)
+        return;
 
-    if(_idx_ptr >= _idx_end) _output.end_input();
-    if(end <= _idx_ptr) return;
+    if (_idx_ptr >= _idx_end)
+        _output.end_input();
+    if (end <= _idx_ptr)
+        return;
     /**/
     size_t t;
-    if(_idx_ptr >= index) t = _idx_ptr - index;
-    else t = 0;
-    for(size_t i = t; i < data.size(); i ++){
-        if(i + index >= _idx_end) break;
+    if (_idx_ptr >= index)
+        t = _idx_ptr - index;
+    else
+        t = 0;
+    for (size_t i = t; i < data.size(); i++) {
+        if (i + index >= _idx_end)
+            break;
         _map[i + index] = data[i];
         _set.insert(i + index);
-
     }
     std::string res;
     size_t idx = _idx_ptr;
-    while(_map.count(idx)){
-        if(idx >= _idx_end) break;
-        //超过 _capacity 容量了 ，不能直接返回， 需要判断map最后一个位置和当前位置哪一个比较大， 再判断是否需要删除
+    while (_map.count(idx)) {
+        if (idx >= _idx_end)
+            break;
+        // 超过 _capacity 容量了 ，不能直接返回， 需要判断map最后一个位置和当前位置哪一个比较大， 再判断是否需要删除
         /*if(_map.size() + _output.buffer_size() > _capacity){
             size_t temp = * _set.rbegin();
             if(temp > idx){
@@ -53,25 +58,26 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
                 _set.erase(temp);
             } else break;
         }*/
-        res += _map[idx ++ ];
+        res += _map[idx++];
     }
     size_t ret = _output.write(res);
-    for(size_t i = 0; i < ret; i ++ ){
+    for (size_t i = 0; i < ret; i++) {
         _map.erase(_idx_ptr + i);
         _set.erase(_idx_ptr + i);
     }
     _idx_ptr += ret;
-    //删除超过字符容量的字符
+    // 删除超过字符容量的字符
     idx = _idx_ptr;
-    while(_map.count(idx + _capacity)){
-        _map.erase(idx);
-        _set.erase(idx ++ );
+    while (_map.count(idx + _capacity)) {
+        _map.erase(idx + _capacity);
+        _set.erase(idx + _capacity);
+        idx++;
     }
-    if(_idx_ptr >= _idx_end) _output.end_input();
+    if (_idx_ptr >= _idx_end)
+        _output.end_input();
 }
-
 
 size_t StreamReassembler::unassembled_bytes() const { return _map.size(); }
 
-//当没有输入状态等待的时候返回 true
+// 当没有输入状态等待的时候返回 true
 bool StreamReassembler::empty() const { return unassembled_bytes() == 0; }
--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -2,8 +2,11 @@
 
 #include <iostream>
 #include <limits>
+#include <sstream>
+#include <string>
 #include <sys/time.h>
-
+#include <thread>
+#include <util/logPoint.cc>
 // Dummy implementation of a TCP connection
 
 // For Lab 4, please replace with a real implementation that passes the
@@ -14,7 +17,7 @@ void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return _receiver.stream_out().remaining_capacity(); }
+size_t TCPConnection::remaining_outbound_capacity() const { return _sender.stream_in().remaining_capacity(); }
 
 size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
 
@@ -43,20 +46,32 @@ void TCPConnection::fill_queue(std::queue<TCPSegment> &segments_out) {
 }
 
 void TCPConnection::send_segments(bool should_reply) {
+    stringstream ss;
     std::queue<TCPSegment> &segments_out = _sender.segments_out();
     if (segments_out.empty() && should_reply) {
+        ss << this_thread::get_id();
+        lprintf(to_string(getpid()).c_str());
+        lprintf("当前segments_out为空\n");
         _sender.send_empty_segment();
     }
-    if (rst_ && should_reply) {
+    if (rst_) {
+        lprintf(to_string(getpid()).c_str());
+        lprintf("出现rst，发送rst报文\n");
         fill_queue(segments_out);
         return;
     }
-    while (!segments_out.empty() && should_reply) {
+    while (!segments_out.empty()) {
+        lprintf(to_string(getpid()).c_str());
+        lprintf("发送非空报文\n");
         fill_queue(segments_out);
     }
 }
 
 void TCPConnection::segment_received(const TCPSegment &seg) {
+    stringstream ss;
+    lprintf(ss.str().c_str());
+    lprintf("调用segment_received\n");
+
     if (!active_) {
         return;
     }
@@ -76,34 +91,58 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
         // 状态转移            state_ = TCPState::State::FIN_WAIT_2;
         _sender.ack_received(seg.header().ackno, seg.header().win);
         if (seg.header().syn && !seg.header().ack) {
+            ss << this_thread::get_id();
+            lprintf(ss.str().c_str());
+            lprintf("经过状态SYN_SENT,转移至SYN_RCVD\t");
             state_ = TCPState::State::SYN_RCVD;
             fill_window(seg.length_in_sequence_space());
         } else {
+            ss << this_thread::get_id();
+            lprintf(ss.str().c_str());
+            lprintf("经过状态SYN_SENT,转移至ESTABLISHED\t");
             state_ = TCPState::State::ESTABLISHED;
             fill_window(seg.length_in_sequence_space());
         }
 
     } else if (state_ == TCPState::State::LISTEN) {
+        ss << this_thread::get_id();
+        lprintf(ss.str().c_str());
+        lprintf("经过状态LISTEN\n");
         if (seg.header().syn) {
             _sender.ack_received(seg.header().ackno, seg.header().win);
             fill_window(seg.length_in_sequence_space());
             // 状态转移
+            ss << this_thread::get_id();
+            lprintf(ss.str().c_str());
+            lprintf("经过LISTEN,转移至SYN_RCVD\n");
             state_ = TCPState::State::SYN_RCVD;
         }
 
     } else if (state_ == TCPState::State::SYN_RCVD) {
+        ss << this_thread::get_id();
+        lprintf(ss.str().c_str());
+        lprintf("经过状态SYN_RCVD\n");
         if (seg.header().rst) {
+            lprintf(ss.str().c_str());
+            lprintf("经过状态SYN_RCVD，转移至LISTEN\n");
             state_ = TCPState::State::LISTEN;
         } else if (seg.header().ack) {
             _sender.ack_received(seg.header().ackno, seg.header().win);
+            lprintf(ss.str().c_str());
+            lprintf("经过状态SYN_RCVD转移至ESTABLISHED\n");
             state_ = TCPState::State::ESTABLISHED;
         }
 
     } else if (state_ == TCPState::State::ESTABLISHED) {
         _sender.ack_received(seg.header().ackno, seg.header().win);
         fill_window(seg.length_in_sequence_space());
+        ss << this_thread::get_id();
+        lprintf(ss.str().c_str());
+        lprintf("当前状态ESTABLISHED，已经确认了该报文\n");
         if (seg.header().fin) {
             _linger_after_streams_finish = false;
+            lprintf(ss.str().c_str());
+            lprintf("经过状态ESTABLISHED,转移至CLOSE_WAIT\n");
             state_ = TCPState::State::CLOSE_WAIT;
         }
 
@@ -111,12 +150,18 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
         // 收到对于fin的确认报文段
         _sender.ack_received(seg.header().ackno, seg.header().win);
         if (seg.header().ack && _sender.bytes_in_flight() == 0 && seg.header().fin) {
+            lprintf(ss.str().c_str());
+            lprintf("经过状态FIN_WAIT_1,转移至TIME_WAIT\n");
             state_ = TCPState::State::TIME_WAIT;
             fill_window(seg.length_in_sequence_space());
         } else if (seg.header().ack && _sender.bytes_in_flight() == 0) {
+            lprintf(ss.str().c_str());
+            lprintf("经过状态FIN_WAIT_1,转移至FIN_WAIT_2\n");
             state_ = TCPState::State::FIN_WAIT_2;
         }  // 收到对方发来的fin，直接CLOSING
         else if (seg.header().fin) {
+            lprintf(ss.str().c_str());
+            lprintf("经过状态FIN_WAIT_1,转移至CLOSING\n");
             state_ = TCPState::State::CLOSING;
             fill_window(seg.length_in_sequence_space());
         }
@@ -145,9 +190,6 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
             state_ = TCPState::State::TIME_WAIT;
             time_wait_start_ = 0;
         }
-        //        else {
-        //            fill_window(seg.length_in_sequence_space());
-        //        }
 
     } else if (state_ == TCPState::State::CLOSE_WAIT) {
         _sender.ack_received(seg.header().ackno, seg.header().win);
@@ -160,9 +202,6 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
             _linger_after_streams_finish = false;
             state_ = TCPState::State::CLOSED;
         }
-        //        else {
-        //            fill_window(seg.length_in_sequence_space());
-        //        }
     } else if (state_ == TCPState::State::TIME_WAIT) {
         if (seg.header().fin) {
             _sender.ack_received(seg.header().ackno, seg.header().win);
@@ -177,17 +216,27 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
             _segments_out.push(segment);
             _sender.segments_out().pop();
         }
-        //        else {
-        //            fill_window(seg.length_in_sequence_space());
-        //        }
     }
     test_end();
 }
 
-bool TCPConnection::active() const { return active_; }
+bool TCPConnection::active() const {
+    if (state_ == TCPState::State::CLOSED || rst_ ||
+        (TCPState::state_summary(_receiver) == TCPReceiverStateSummary::FIN_RECV &&
+         TCPState::state_summary(_sender) == TCPSenderStateSummary::FIN_ACKED &&
+         time_since_last_segment_received() >= 10 * _cfg.rt_timeout)) {
+        return false;
+    }
+    return active_;
+}
 
 size_t TCPConnection::write(const string &data) {
     // 先写入_sender里面的bytestream
+    stringstream ss;
+    lprintf(ss.str().c_str());
+    lprintf("写入数据");
+    lprintf(data.c_str());
+    lprintf("\n");
     size_t write_len = _sender.stream_in().write(std::move(data));
     fill_window(true);
     test_end();
@@ -196,11 +245,22 @@ size_t TCPConnection::write(const string &data) {
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
 void TCPConnection::tick(const size_t ms_since_last_tick) {
+    if (state_ == TCPState::State::LISTEN)
+        return;
     time_last_segment_received_ += ms_since_last_tick;
+
+    stringstream ss;
+    ss << this_thread::get_id();
+    lprintf(ss.str().c_str());
+    lprintf("调用tick(");
+    lprintf(to_string(ms_since_last_tick).c_str());
+    lprintf(")\n");
 
     if (state_ == TCPState::State::TIME_WAIT) {
         time_wait_start_ += ms_since_last_tick;
         if (time_wait_start_ >= TCPConfig::TIMEOUT_DFLT * 10) {
+            lprintf(ss.str().c_str());
+            lprintf("time_wait状态过渡到closed");
             active_ = false;
             _linger_after_streams_finish = false;
             state_ = TCPState::State::CLOSED;
@@ -210,6 +270,10 @@ void TCPConnection::tick(const size_t ms_since_last_tick) {
 
     if (_sender.tick(ms_since_last_tick)) {
         if (_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS) {
+            lprintf(ss.str().c_str());
+            lprintf("当前状态：");
+            lprintf(state_.name().c_str());
+            lprintf("超时重传次数过多，设置rst\n");
             set_rst();
             return;
         }
@@ -221,17 +285,18 @@ void TCPConnection::tick(const size_t ms_since_last_tick) {
 
 void TCPConnection::end_input_stream() {
     _sender.stream_in().end_input();
+    /*存在一种情况，窗口已经满了，fin实际上是不能马上传出去的*/
 
-    if (state_ == TCPState::State::ESTABLISHED) {
-        fill_window(true);
+    if (state_ == TCPState::State::ESTABLISHED || state_ == TCPState::State::CLOSE_WAIT)
+        fill_window(false);
+    // 主动关闭
+    if (state_ == TCPState::State::ESTABLISHED && TCPState::state_summary(_sender) == TCPSenderStateSummary::FIN_SENT) {
         state_ = TCPState::State::FIN_WAIT_1;
-    } else if (state_ == TCPState::State::CLOSE_WAIT) {
-        fill_window(true);
+        return;
+    }
+    // 被动关闭
+    if (state_ == TCPState::State::CLOSE_WAIT && TCPState::state_summary(_sender) == TCPSenderStateSummary::FIN_SENT) {
         state_ = TCPState::State::LAST_ACK;
-    } else if (state_ == TCPState::State::LISTEN) {
-        state_ = TCPState::State::CLOSED;
-    } else if (state_ == TCPState::State::SYN_SENT) {
-        state_ = TCPState::State::CLOSED;
     }
 
     test_end();
@@ -242,7 +307,11 @@ void TCPConnection::connect() {
     if (_sender.next_seqno_absolute() != 0) {
         return;
     }
-
+    stringstream ss;
+    lprintf("\n\n\n\n\n");
+    ss << this_thread::get_id();
+    lprintf(ss.str().c_str());
+    lprintf("请求connect\n");
     active_ = true;
     // 三次握手
     _sender.fill_window();
@@ -264,6 +333,16 @@ void TCPConnection::test_end() {
             }
         }
     }
+
+    if (_receiver.stream_out().input_ended() && !_sender.stream_in().eof() && _sender.next_seqno_absolute() > 0) {
+        _linger_after_streams_finish = false;
+    } else if (_receiver.stream_out().eof() && _sender.stream_in().eof() && unassembled_bytes() == 0 &&
+               bytes_in_flight() == 0 && TCPState::state_summary(_sender) == TCPSenderStateSummary::SYN_SENT) {
+        if (!_linger_after_streams_finish)
+            active_ = false;
+        else if (time_last_segment_received_ >= 10 * _cfg.rt_timeout)
+            active_ = false;
+    }
 }
 
 void TCPConnection::set_rst() {
@@ -272,14 +351,13 @@ void TCPConnection::set_rst() {
     _linger_after_streams_finish = false;
     _sender.stream_in().set_error();
     _receiver.stream_out().set_error();
-    if (state_ == TCPState::State::ESTABLISHED) {
-        send_segments(true);
-    }
+    send_segments(true);
 }
 
 TCPConnection::~TCPConnection() {
     try {
         if (active()) {
+            lprintf("unclean shutdown\n");
             cerr << "Warning: Unclean shutdown of TCPConnection\n";
             // Your code here: need to send a RST segment to the peer
             set_rst();
